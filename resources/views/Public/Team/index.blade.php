8a66cc4faa400c13315851c94f532e25c40fe851
itsekrie <fahriantekrie@students.undip.ac.id>
2024-10-16 17:42:21 +0700

    dash_header_etc

--- resources/views/Public/Team/index.blade.php
+++ resources/views/Public/Team/index.blade.php
@@ -7,7 +7,7 @@
 <section>
     <div>
         @foreach($teams as $tim)
-        <a href="{{route('Team.show', $tim->Slug)}}" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <a href="{{route('Teams.show', $tim->Slug)}}" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <h5 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
             {{$tim->Team_Name}}
             </h5>
